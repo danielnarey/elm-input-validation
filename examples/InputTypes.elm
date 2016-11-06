c99adc4d432ba7cd4b00f1761f92a2d0f2691c79
Daniel C Narey <danielnarey@users.noreply.github.com>
2016-11-06 18:02:04 -0500

    First commit

---
+++ examples/InputTypes.elm
@@ -0,0 +1,179 @@
+import InputValidation as Validate exposing (TypedInput(..))
+import Html exposing (Html)
+import Html.Attributes as Attr
+import Html.Events as Events
+import Html.App as App
+import Json.Decode as Json
+import List
+import String
+
+
+main =
+  App.beginnerProgram
+    { model = userInput
+    , view = view
+    , update = update
+    }
+
+
+-- MODEL
+
+type alias UserInput =
+  { name : Maybe String
+  , age : Maybe Int
+  , pi : Maybe Float
+  , answer : Maybe Bool
+  , chars : Maybe (List Char)
+  }
+
+userInput =
+  { name = Nothing
+  , age = Nothing
+  , pi = Nothing
+  , answer = Nothing
+  , chars = Nothing
+  }
+
+
+-- UPDATE
+
+type CapturedValue a
+  = Name (TypedInput a)
+  | Age (TypedInput a)
+  | Pi (TypedInput a)
+  | Answer (TypedInput a)
+  | Chars (TypedInput a)
+
+
+update : CapturedValue (List Char) -> UserInput -> UserInput
+update capturedValue userInput =
+  case capturedValue of
+    Name value ->
+      { userInput
+      | name =
+          value
+            |> Validate.readStringInput
+            |> Result.toMaybe
+      }
+
+    Age value ->
+      { userInput
+      | age =
+          value
+            |> Validate.readIntInput
+            |> Result.toMaybe
+      }
+
+    Pi value ->
+      { userInput
+      | pi =
+          value
+            |> Validate.readFloatInput
+            |> Result.toMaybe
+      }
+
+    Answer value ->
+      { userInput
+      | answer =
+          value
+            |> Validate.readBoolInput
+            |> Result.toMaybe
+      }
+
+    Chars value ->
+      { userInput
+      | chars =
+          value
+            |> Validate.readCustomInput
+            |> Result.toMaybe
+      }
+
+
+-- VIEW
+
+view : UserInput -> Html (CapturedValue (List Char))
+view userInput =
+  let
+    row1 =
+      [ "What is your name?"
+        |> Html.text
+      , StringInput
+        |> inputField Name
+      , userInput.name
+        |> Maybe.map (\s -> "\"" ++ s ++ "\"")
+        |> Maybe.withDefault "Expecting a `String`"
+        |> Html.text
+      ]
+
+    row2 =
+      [ "How old are you?"
+        |> Html.text
+      , IntInput
+        |> inputField Age
+      , userInput.age
+        |> Maybe.map toString
+        |> Maybe.withDefault "Expecting an `Int`"
+        |> Html.text
+      ]
+
+    row3 =
+      [ "Enter the first few digits of Pi:"
+        |> Html.text
+      , FloatInput
+        |> inputField Pi
+      , userInput.pi
+        |> Maybe.map toString
+        |> Maybe.withDefault "Expecting a `Float`"
+        |> Html.text
+      ]
+
+    row4 =
+      [ "Type the word PLATYPUS:"
+        |> Html.text
+      , BoolInput (\s -> String.toLower s == "platypus")
+        |> inputField Answer
+      , userInput.answer
+        |> Maybe.map (\b -> if b then "Correct" else "Incorrect")
+        |> Maybe.withDefault "Expecting a `Bool` result"
+        |> Html.text
+      ]
+
+    row5 =
+      [ "Type some characters!"
+        |> Html.text
+      , CustomInput (Json.string |> Json.map String.toList)
+        |> inputField Chars
+      , userInput.chars
+        |> Maybe.map (\list -> list |> List.intersperse ',' |> String.fromList)
+        |> Maybe.map (\s -> "[" ++ s ++ "]")
+        |> Maybe.withDefault "Expecting a `Char` sequence"
+        |> Html.text
+      ]
+
+
+  in
+    [ row1
+    , row2
+    , row3
+    , row4
+    , row5
+    ]
+      |> List.map tableRow
+      |> Html.table []
+
+
+tableRow : List (Html msg) -> Html msg
+tableRow elementList =
+  elementList
+    |> List.map (\elem -> Html.td [] [elem])
+    |> Html.tr []
+
+
+inputField : (TypedInput a -> CapturedValue a) -> (Json.Value -> TypedInput a) -> Html (CapturedValue a)
+inputField captureKey inputKey =
+  []
+    |> Html.input
+      [ Attr.type' "text"
+      , inputKey
+        |> Validate.captureOnInput captureKey
+      ]
---
+++ src/InputValidation.elm
@@ -0,0 +1,188 @@
+module InputValidation exposing (..)
+
+{-| ## Type-validation for user input from text, numeric, and custom fields
+
+This library provides a consistent way of handling type-validation for user
+input. The type of input expected is specified when constructing an input
+element in the Elm program's view, and the corresponding reader function is
+called when program updates in response to input. If the input does not match
+the expected type, or the reader function called does not correspond to the type
+expected, an error results.
+
+# Value Representation
+@docs TypedInput
+
+# Capturing Input
+@docs captureOnInput
+
+# Reading Input with Type Validation
+@docs readStringInput, readIntInput, readFloatInput, readBoolInput
+@docs readCustomInput
+
+# Reading Input without Type Validation
+@docs readInputAsString
+
+-}
+
+import Toolkit.Operators exposing (..)
+import Toolkit.Helpers exposing (thenTry)
+import Json.Decode as Json exposing (Decoder)
+import Html exposing (Attribute)
+import Html.Events as Events
+import String
+
+
+-- VALUE REPRESENTATION
+
+{-| Represents a JavaScript value with a type specification.
+
+`BoolInput` requires an expression that takes a string and returns a `Bool`.
+`CustomInput` requires a `Json`
+[`Decoder`](http://package.elm-lang.org/packages/elm-lang/core/latest/Json-Decode#Decoder).
+`Fail` captures no input and may be used for error handling.
+-}
+type TypedInput a
+  = StringInput Json.Value
+  | IntInput Json.Value
+  | FloatInput Json.Value
+  | BoolInput (String -> Bool) Json.Value
+  | CustomInput (Decoder a) Json.Value
+  | Fail
+
+
+-- CAPTURING INPUT
+
+{-| Capture input from a field as `TypedInput`
+
+    type Msg
+      = Name (TypedInput a)
+    ...
+
+    input [ type' "text", captureOnInput Name StringInput ] []
+
+-}
+captureOnInput : (TypedInput a -> msg) -> (Json.Value -> TypedInput a) -> Attribute msg
+captureOnInput captureKey inputKey =
+  Json.value
+    |> Json.at ["target", "value"]
+    |> Json.map (\v -> inputKey v)
+    |> Json.map captureKey
+    |> Events.on "input"
+
+
+-- READING INPUT WITH TYPE VALIDATION
+
+{-| Decode a `StringInput` value as a string; return an error message if the
+decoder fails or if the argument is a type other than `StringInput`.
+
+-}
+readStringInput : TypedInput a -> Result String String
+readStringInput typedInput =
+  case typedInput of
+    StringInput jsonValue ->
+      jsonValue
+        |> Json.decodeValue Json.string
+
+    _ ->
+      Err "Expecting `StringInput`"
+
+
+{-| Decode an `IntInput` value as an string, then attempt to convert the string
+to an `Int`; return an error message if the string decoder fails, if type
+conversion fails, or if the argument is a type other than `IntInput`.
+
+-}
+readIntInput : TypedInput a -> Result String Int
+readIntInput typedInput =
+  case typedInput of
+    IntInput jsonValue ->
+      jsonValue
+        |> Json.decodeValue Json.string
+        |> thenTry String.toInt
+
+    _ ->
+      Err "Expecting `IntInput`"
+
+
+{-| Decode a `FloatInput` value as a string, then attempt to convert the string
+to a `Float`; return an error message if the string decoder fails, if type
+conversion fails, or if the argument is a type other than `FloatInput`.
+
+-}
+readFloatInput : TypedInput a -> Result String Float
+readFloatInput typedInput =
+  case typedInput of
+    FloatInput jsonValue ->
+      jsonValue
+        |> Json.decodeValue Json.string
+        |> thenTry String.toFloat
+
+    _ ->
+      Err "Expecting `FloatInput`"
+
+
+{-| Decode a `BoolInput` value as a string, then pass the string to a custom
+expression that will return a `Bool`; return an error message if the string
+decoder fails or if the argument is a type other than `BoolInput`.
+
+-}
+readBoolInput : TypedInput a -> Result String Bool
+readBoolInput typedInput =
+  case typedInput of
+    BoolInput expression jsonValue ->
+      jsonValue
+        |> Json.decodeValue Json.string
+        !|> expression
+
+
+    _ ->
+      Err "Expecting `BoolInput`"
+
+
+{-| Decode a `CustomInput` value with a custom `Json`
+[`Decoder`](http://package.elm-lang.org/packages/elm-lang/core/latest/Json-Decode#Decoder);
+return an error message if the decoder fails or if the argument is a type other
+than `CustomInput`.
+-}
+readCustomInput : TypedInput a -> Result String a
+readCustomInput typedInput =
+  case typedInput of
+    CustomInput decoder jsonValue ->
+      jsonValue
+        |> Json.decodeValue decoder
+
+    _ ->
+      Err "Expecting `CustomInput`"
+
+
+-- READING INPUT WITHOUT TYPE VALIDATION
+
+{-| Decode any `TypedInput` value as a string, or return an error message if the
+decoder fails. Useful for debugging.
+
+-}
+readInputAsString : TypedInput a -> Result String String
+readInputAsString typedInput =
+  case typedInput of
+    StringInput jsonValue ->
+      jsonValue
+        |> Json.decodeValue Json.string
+
+    IntInput jsonValue ->
+      jsonValue
+        |> Json.decodeValue Json.string
+
+    FloatInput jsonValue ->
+      jsonValue
+        |> Json.decodeValue Json.string
+
+    BoolInput expression jsonValue ->
+      jsonValue
+        |> Json.decodeValue Json.string
+
+    CustomInput decoder jsonValue ->
+      jsonValue
+        |> Json.decodeValue Json.string
+
+    Fail ->
+      Err "Something went wrong: Trying to read `Fail` as input"
